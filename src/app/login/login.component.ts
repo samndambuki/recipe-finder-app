ccae042d0350bd7fd86511da301973a8f1b72838
samndambuki <sam.ndambuki08@gmail.com>
2023-07-18 22:13:53 +0300

    sign up and login work but I have to reload the page in order to navigate to the dashboard

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,14 +1,66 @@
+//import the necessary dependencies for the component to work correctly
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
+import { UserService } from '../services/user.service';
+import { FormsModule } from '@angular/forms';
 
+//component decorator which defines the meta data for our application
 @Component({
+  //special nick name for our component. It can be used as a directive
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule,RouterModule],
+  imports: [CommonModule,RouterModule,FormsModule],
+  //tell angular where to find the html template
   templateUrl: './login.component.html',
+  //tell angular where to find the styles or our component
   styleUrls: ['./login.component.css']
 })
+
+
 export class LoginComponent {
 
+  //declare necessary properties to hold values enterd by the user in the login form
+  email!:string;
+  password!:string;
+  error!:string;
+
+  //create a component that gets called when our component is created
+  constructor(private router:Router,private userService:UserService){}
+
+  //function that gets called when a user clicks a button to log in 
+  //void - doesnt return anything
+  //when the user clicks the login button, this function will be executed
+  loginUser():void{
+    console.log('login button clicked');
+    
+    //we use userService to make a request to get user data based on their email
+    //we subscribe to the response of request which means we wait for it to complete and get the result
+    //call a method getUserByEmail and pass in users email as an argument
+    //takes two functions as an argument.
+    // one to handle the success case and the other to handle errors
+    this.userService.getUserByEmail(this.email).subscribe((user)=>{
+
+      //if user is found and the password matches the entered passsword
+      if(user && user.password === this.password){
+        //we log a success message with user details 
+        console.log("User logged in successfully",user);
+        //store their credentials in local browser
+        localStorage.setItem('credentials',JSON.stringify(user))
+        //navigate them to dashboard
+        this.router.navigate(['/dashboard'])
+      }else{
+        //if the user is not found or the password does not match
+        //we set an error messsage in the error variable
+        this.error = 'Invalid email or password'
+      }
+    }, 
+    //if there is an eror while getting the users data
+    (error)=>{
+      //we log the erorr in the console
+      console.error("Error logging in",error)
+      //set an error message to be displayed
+      this.error = "An error ocurred while logging in"
+    })
+  }
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,42 +1,51 @@
 //import the necessary dependecnies
-import { HttpClient } from "@angular/common/http";
-import { Injectable } from "@angular/core";
-import { Observable } from "rxjs";
-import { User } from "../interfaces/User";
-
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../interfaces/User';
 
 //marks UserService as an injectable service
 @Injectable({
-    //makes this service available throughout the application
-    providedIn:'root'
+  //makes this service available throughout the application
+  providedIn: 'root',
 })
 
 //UserService is exported and defined
-export class UserService{
-    
-    //stores base Url for the api
-    private baseUrl = 'http://localhost:3000';
+export class UserService {
+  //stores base Url for the api
+  private baseUrl = 'http://localhost:3000';
 
-    //HttpClient is injected as a dependency
-    constructor(private http:HttpClient){}
+  //HttpClient is injected as a dependency
+  constructor(private http: HttpClient) {}
 
-    //createUser method takes in user as an object of type User
-    //user object represents data we want to send to the api for creation
-    //represents the data of the user we want  to create
-    //returns an observable of type user to represent asynchronous nature of http call
-    //observable  - container represents stream of asynchronous data
-    //reponse should be of type user
-    createUser(user:User):Observable<User>{
-        //post request is made using http client 
-        //htpp post request made using http client to the url
-        //user object passed as a request payload - data sent from client to server 
-        //user object passed as a second argument
-        //conatins users information such as name, email, password
-        //expects server to respond to respond with a User object
-        //returns an observable of type User
-        //by subscribing to the observable you can you can obtain th response from the server
-        //and perform necessary actions based on the result
-        //this.http - instance of the http client tahat was injected into the User Service class
-        return this.http.post<User>(`${this.baseUrl}/users`,user);
-    }
+  //createUser method takes in user as an object of type User
+  //user object represents data we want to send to the api for creation
+  //represents the data of the user we want  to create
+  //returns an observable of type user to represent asynchronous nature of http call
+  //observable  - container represents stream of asynchronous data
+  //reponse should be of type user
+  createUser(user: User): Observable<User> {
+    //post request is made using http client
+    //htpp post request made using http client to the url
+    //user object passed as a request payload - data sent from client to server
+    //user object passed as a second argument
+    //conatins users information such as name, email, password
+    //expects server to respond to respond with a User object
+    //returns an observable of type User
+    //by subscribing to the observable you can you can obtain th response from the server
+    //and perform necessary actions based on the result
+    //this.http - instance of the http client tahat was injected into the User Service class
+    return this.http.post<User>(`${this.baseUrl}/users`, user);
+  }
+
+  //method which takes an email parameter of type string
+  //method returns an observable<User> which is a stream of user objects
+  getUserByEmail(email: string): Observable<User> {
+    //this line uses http property which is an instance of HttpClient to make a Http Get request
+    //the generic parameter <User> is the expected response type
+    //the get method is called with the following arguments
+    //the url - this constructs the url by concatinating the base url with the path users
+    //and the query parameter email
+    return this.http.get<User>(`${this.baseUrl}/users?email=${email}`);
+  }
 }
