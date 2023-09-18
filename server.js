7763bf5cf2d3ab6709b6b9090964b8eadd2c955c
samndambuki <sam.ndambuki08@gmail.com>
2023-09-18 12:57:10 +0300

    modified server.js

--- server.js
+++ server.js
@@ -1,27 +1,21 @@
-const express = require('express');
-const path = require('path');
-const { exec } = require('child_process');
+const express = require("express");
+const path = require("path");
+const { exec } = require("child_process");
 
 const app = express();
 
-// Serve the Angular app from the 'dist' directory
-app.use(express.static(path.join(__dirname, 'dist')));
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + "/dist/recipe-finder-app"));
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname + "/dist/recipe-finder-app/index.html"));
+});
 
+app.listen(process.env.PORT || 8080);
 // Start json-server
-exec('json-server --watch db.json', (error, stdout, stderr) => {
+exec("json-server --watch db.json", (error, stdout, stderr) => {
   if (error) {
     console.error(`Error running json-server: ${error}`);
     return;
   }
   console.log(`json-server is running: ${stdout}`);
-});
-
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/frontend'));
-app.get('/*', function(req,res) 
-{ res.sendFile(path.join(__dirname+'/dist/frontend/index.html'));}); 
-
-const port = process.env.PORT || 8080;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
 });
